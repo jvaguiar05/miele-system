2da3dd30db5a995940125eb3832e3c9b77b1cddd
Joao Vitor <jvads2005@gmail.com>
2025-11-21 14:57:19 -0300

    chore: refactor dependencies structure for improved readability and maintainability. setup jazzmin library for admin-backoffice

--- backend/core/settings/base.py
+++ backend/core/settings/base.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["*"])
 
 # Applications
 INSTALLED_APPS = [
+    "jazzmin",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
